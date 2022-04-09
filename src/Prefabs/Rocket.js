1bf31f27dae93b3c6dfe175ba0a6922f131cc9f4
JodagonDrago <josephHaloman0@gmail.com>
2022-04-09 14:43:46 -0700

    Finished

--- src/Prefabs/Rocket.js
+++ src/Prefabs/Rocket.js
@@ -9,6 +9,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       //some parameters
       this.isFiring = false;
       this.moveSpeed = 2;
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update(){
@@ -22,8 +23,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
 
         //if fired, move up
@@ -36,5 +38,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false;
             this.y = game.config.height - borderUISize - borderPadding;
         }
+    }
+
+    //reset rocket
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
