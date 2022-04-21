236bcf571f995102276c86c3d28b69e9d6735e32
JodagonDrago <josephhaloman0@gmail.com>
2022-04-21 09:32:19 -0700

    Start of changes to base game

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 let config = {
     type: Phaser.CANVAS,
-    width: 640,
-    height: 480,
+    width: 1600,
+    height: 900,
+    autoCenter: true, //this centers the game in the window
     scene: [Menu, Play]
 }
 let game = new Phaser.Game(config)
 
 // reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyF, keyR, keyLEFT, keyRIGHT, keyUP, keyDOWN;
+
+//reserve global variables
+let life;
 
 //set UI sizes
 let borderUISize = game.config.height / 15;
