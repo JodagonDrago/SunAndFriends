cfcecbac3c41da63d0041afae76b53971a083557
JodagonDrago <josephHaloman0@gmail.com>
2022-04-22 19:55:27 -0700

    Acceleration functional

--- src/Prefabs/Sun.js
+++ src/Prefabs/Sun.js
@@ -8,23 +8,40 @@ class Sun extends Phaser.GameObjects.Sprite {
 
       //some parameters
       this.isFiring = false;
-      this.moveSpeed = 2;
+      this.moveSpeedX = 0.0;
+      this.moveSpeedY = 0.0;
+      this.maxSpeed = 4.0;
       this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update(){
         //movement
-        if(keyLEFT.isDown && this.x >= borderUISize + this.width){
-            this.x -=this.moveSpeed;
+        this.x +=this.moveSpeedX;
+        this.y +=this.moveSpeedY;
+        if(keyLEFT.isDown && this.x >= borderUISize + this.width && this.moveSpeedX > -this.maxSpeed){
+            this.moveSpeedX -= 0.2;
         }
-        if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-            this.x +=this.moveSpeed;
+        if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width && this.moveSpeedX < this.maxSpeed){
+            this.moveSpeedX += 0.2;
         }
-        if(keyUP.isDown && this.y >= borderUISize + this.height){
-            this.y -=this.moveSpeed;
+        if(keyUP.isDown && this.y >= borderUISize && this.moveSpeedY > -this.maxSpeed){
+            this.moveSpeedY -= 0.2;
         }
-        if(keyDOWN.isDown && this.y <= game.config.height - borderUISize - this.height){
-            this.y +=this.moveSpeed;
+        if(keyDOWN.isDown && this.y <= game.config.height - borderUISize - this.height && this.moveSpeedY < this.maxSpeed){
+            this.moveSpeedY += 0.2;
+        }
+        //stop acceleration if hitting a border
+        if(this.x < borderUISize + this.width){
+            this.moveSpeedX = 0.2;
+        }
+        if(this.x > game.config.width - borderUISize - this.width){
+            this.moveSpeedX = -0.2;
+        }
+        if(this.y < borderUISize){
+            this.moveSpeedY = 0.2;
+        }
+        if(this.y > game.config.height - borderUISize - this.height){
+            this.moveSpeedY = -0.2;
         }
     }
 
