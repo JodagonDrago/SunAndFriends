d9c8c8d8cdc0940aa76208ade7ed22f4e43af36a
JodagonDrago <josephHaloman0@gmail.com>
2022-04-29 15:40:39 -0700

    Changed background movement direction

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -98,7 +98,7 @@ class Play extends Phaser.Scene{
             this.scene.start("menuScene");
         }
 
-        this.starfield.tilePositionX -= 4;
+        this.starfield.tilePositionX += 4;
 
         if (!this.gameOver){
             // update sun sprite
