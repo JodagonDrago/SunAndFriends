0f1ffe76ab48c64fa02d3a0ff2905371d8a8b6fd
JodagonDrago <josephHaloman0@gmail.com>
2022-05-01 15:23:11 -0700

    Final Polish

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -15,7 +15,7 @@ class Play extends Phaser.Scene{
         this.load.image('parallax', './assets/parallax.png')
 
         // load spritesheet
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 100, frameHeight: 100, startFrame: 0, endFrame: 9});
     }
 
     create(){
@@ -156,6 +156,12 @@ class Play extends Phaser.Scene{
         }
 
         // check collisions for asteroids
+        if(this.checkCollision(this.sun, this.asteroid05)) {
+            this.hitAsteroid(this.asteroid05);
+        }
+        if(this.checkCollision(this.sun, this.asteroid04)) {
+            this.hitAsteroid(this.asteroid04);
+        }
         if(this.checkCollision(this.sun, this.asteroid03)) {
             this.hitAsteroid(this.asteroid03);
         }
