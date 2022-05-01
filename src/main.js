c3c95a0ed999617c58d1fd9931a71c1c89dfa488
JodagonDrago <josephHaloman0@gmail.com>
2022-05-01 15:36:15 -0700

    Message added to main

--- src/main.js
+++ src/main.js
@@ -16,3 +16,7 @@ let life, planetCount, music, musicOff;
 //set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+//Creative Tilt:
+//Technically Interesting: Added bodies that follow the player and rotate about the player at various speeds to make a solar system, and also serve as earnable lives
+//Visual Style: We implemented paralax movement in the scrolling stars, as well as 'friends' orbitting bodies around the sun
